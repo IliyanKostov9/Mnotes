d08cae6c0128bacb8b3afe009227a2768556ffd4
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2024-01-20 23:01:19 +0200

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "mnotes": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
